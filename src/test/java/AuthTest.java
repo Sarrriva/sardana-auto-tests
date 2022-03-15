31236e57f44151d138f45ad87c9e1c60ab0a26c6
Sarrriva <s.ivanova.kazanexpress@gmail.com>
2022-03-15 23:35:42 +0300

    Добавила тесты

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,42 +1,43 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import pages.GitHubPages;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class AuthTest {
 
     @Test
     public void shouldAuthorizeTest() {
-        //1. Открыть страницу https://github.com/
-
-        open("https://github.com/");
+        //1. Открыть страницу https://github.com
+        open("https://github.com");
 
         //2. Кликнуть на кнопку Sign in
-
-        $("[href='/login']").click();
+        GitHubPages.gitHubWelcomePage.loginButton()
+                .click();
 
         //3. Заполнить поля Логина и Пароля
-
-        $("[id='login_field']").sendKeys("Sarrriva");
-        $("[id='password']").sendKeys("Patipuka123");
+        GitHubPages.gitHubLoginPage.loginField()
+                .sendKeys("Sarrriva");
+        GitHubPages.gitHubLoginPage.passwordField()
+                .sendKeys("Patipuka123");
 
         //4.Нажать кнопку Sign in
-
-        $(".js-sign-in-button").click();
+        GitHubPages.gitHubLoginPage.signInButton()
+                .click();
 
         //5. Проверить авторизацию
-
-        $(".Header").shouldBe(Condition.visible);
+        GitHubPages.gitHubMainPage.profileHeaderBlock()
+                .shouldBe(Condition.visible);
 
         //6. Перейти на страницу профиля
-
-        $("[aria-label='View profile and more']").click();
-        $(byText ("Your profile")).click();
+        GitHubPages.gitHubMainPage.avatarButton()
+                .click();
+        GitHubPages.gitHubMainPage.myProfileButton()
+                .click();
 
         //7. Проверить открытие страницы профиля
-
-        $(".vcard-names").shouldBe(Condition.visible);
+        GitHubPages.gitHubProfilePage.userProfileName()
+                .shouldBe(Condition.visible);
     }
+
 }
