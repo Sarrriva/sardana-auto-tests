b7bb79d4a3b65257a75bc9a3d6f595ac130df823
Sarrriva <s.ivanova.kazanexpress@gmail.com>
2022-03-18 00:14:01 +0300

    Добавила аннотации и степы

--- src/test/java/GitHubJUnitRepositoryTest.java
+++ src/test/java/GitHubJUnitRepositoryTest.java
@@ -1,3 +1,6 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -5,37 +8,49 @@ import pages.GitHubPages;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+@Feature("Github")
+@Owner("Sarrriva")
 
 public class GitHubJUnitRepositoryTest {
 
     @BeforeEach
     public void before() {
-        //1. Открыть страницу https://github.com/junit-team/junit4
-        open("https://github.com/junit-team/junit4");
+        step("Открыть страницу https://github.com/junit-team/junit4", () -> {
+            open("https://github.com/junit-team/junit4");
+        });
     }
 
     @Test
+    @Story("Смена ветки")
     public void shouldSwitchToFixturesBranch() {
-        //2. Кликнуть на кнопку выбора ветки
-        GitHubPages.gitHubMainPage.branchButton().click();
+        step("Кликнуть на кнопку выбора ветки", () -> {
+            GitHubPages.gitHubMainPage.branchButton().click();
+        });
 
-        //3. Выбрать ветку с именем fixtures
-        GitHubPages.gitHubMainPage.fixturesBranchLink().click();
+        step("Выбрать ветку с именем fixtures", () -> {
+            GitHubPages.gitHubMainPage.fixturesBranchLink().click();
+        });
 
-        //4. Проверить название текущей ветки на кнопке выбора ветки
-        GitHubPages.gitHubMainPage.branchButton().shouldHave(text("fixtures"));
+        step("Проверить название текущей ветки на кнопке выбора ветки", () -> {
+            GitHubPages.gitHubMainPage.branchButton().shouldHave(text("fixtures"));
+        });
     }
 
     @Test
+    @Story("Поиск релиза")
     public void shouldSearchReleaseByName() {
-        //2. Кликнуть на ссылку релизов
-        GitHubPages.gitHubMainPage.releasesLink().click();
+        step("Кликнуть на ссылку релизов", () -> {
+            GitHubPages.gitHubMainPage.releasesLink().click();
+        });
 
-        //3. Ввести в поле для поиска текст "4.12"
-        GitHubPages.gitHubReleasesPage.searchReleasesField().sendKeys("4.12" + Keys.ENTER);
+        step("Ввести в поле для поиска текст '4.12'", () -> {
+            GitHubPages.gitHubReleasesPage.searchReleasesField().sendKeys("4.12" + Keys.ENTER);
+        });
 
-        //4. Проверить содержимое первого помеченного результата поиска
-        GitHubPages.gitHubReleasesPage.searchResultFirstMarkedEntry().shouldHave(text("4.12"));
+        step("Проверить содержимое первого помеченного результата поиска", () -> {
+            GitHubPages.gitHubReleasesPage.searchResultFirstMarkedEntry().shouldHave(text("4.12"));
+        });
     }
-
 }
